a4cb964d586f026b71aed45e3817a7a7a5919bd8
zoe-c <find.zoec@gmail.com>
2017-06-24 20:52:55 -0400

    bodyParser added/ initiated. input is printing! figure out how to stop reprint on refresh. is this why/ when validator comes in?

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+//List :require(ments)
 var express = require('express');
 var bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 const mustacheExpress = require('mustache-express');
-// var expressValidator = require('express-validator');
 
+//Create App
 var app = express();
 
-// scaffold
+// create var for input
+var toDo = [];
+
+// scaffold / set view engine
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
@@ -13,42 +18,29 @@ app.set('view engine', 'mustache');
 // styles
 app.use(express.static('public'));
 
+//set up app to use bodyParser and return JSON. can now refer to your render content as req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+// set extended to true to be able to parse nested objects, false = just arrays and strings.
+
+
 // content + data
-const complete = {
-   complete: [
-     {"task":"Make A To Do List", "status": "complete"} ,
-     {"task":"Get It To Render", "status": "complete"} ,
-     {"task":"Add Strike Through", "status": "complete"} ,
-]};
-
-
+// const complete = {
+//    complete: [
+//      {"task":"Make A To Do List", "status": "complete"} ,
+//      {"task":"Get It To Render", "status": "complete"} ,
+//      {"task":"Add Strike Through", "status": "complete"} ,
+// ]};
 
 app.get("/", function (req, res) {
-  res.render('index', complete);
+  res.render('index', {toDo: toDo});
 });
 
-app.post("/addItem", function (req, res) {
-  document.querySelector('ul').push(req.body.todo);
-  res.redirect('/');
-})
-
-
-
-// PARSER
-// Set app to use bodyParser()` middleware.
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-//'extended: false' parses strings and arrays.
-//'extended: true' parses nested objects
-//'expressValidator' must come after 'bodyParser', since data must be parsed first!
-// app.use(expressValidator());
-
-
-
-
-
-
-
+app.post("/", function (req,res){
+   let addTask = {"task": req.body.task};
+   toDo.push(addTask);
+   res.render('index', {toDo: toDo});
+});
 
 app.listen(3000, function(){
   console.log('Started express application!')
